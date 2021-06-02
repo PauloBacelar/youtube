1b3c9b785d75db06cafaa84ea5df3e5da2b36754
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-06-02 07:20:57 -0300

    Created handleFormSubmit function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,10 +7,14 @@ class SearchBar extends React.Component {
     this.setState({ term: event.target.value });
   };
 
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.handleFormSubmit}>
           <div className="field">
             <label>Video Search</label>
             <input
