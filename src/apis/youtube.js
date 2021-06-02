6924f5494465a8bfcf8eb80055bc58f52ed8580a
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-06-02 17:05:13 -0300

    Made axios setup in youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyATIfDQXUCD9yHQCIsXdlGrcDHNdfevWdY";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
