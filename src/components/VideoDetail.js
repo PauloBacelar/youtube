d948b09a082fbadb78d8d8c6b6b3da09814f4c97
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-06-03 09:27:02 -0300

    VVideo

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 const VideoDetail = (props) => {
+  const videoSrc = props.video
+    ? `https://www.youtube.com/embed/${props.video.id.videoId}`
+    : null;
+
   return (
     <div>
       {props.video ? (
-        <div className="ui segment">
-          <h4 className="ui header">{props.video.snippet.title}</h4>
-          <p>{props.video.snippet.description}</p>
+        <div>
+          <div className="ui embed">
+            <iframe src={videoSrc} />
+          </div>
+
+          <div className="ui segment">
+            <h4 className="ui header">{props.video.snippet.title}</h4>
+            <p>{props.video.snippet.description}</p>
+          </div>
         </div>
       ) : null}
     </div>
