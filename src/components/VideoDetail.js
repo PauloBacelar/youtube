30800dd573991e14f927ce8720429466c695488a
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-06-03 09:16:46 -0300

    Shows video title and description

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const VideoDetail = (props) => {
-  return <div>{props.video ? props.video.snippet.title : null}</div>;
+  return (
+    <div>
+      {props.video ? (
+        <div className="ui segment">
+          <h4 className="ui header">{props.video.snippet.title}</h4>
+          <p>{props.video.snippet.description}</p>
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default VideoDetail;
